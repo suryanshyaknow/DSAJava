3d4c305fabe7c723aef77955bc19707cf2471f69
Suryansh Grover <suryanshgrover1999@gmail.com>
2025-03-06 03:51:00 +0530

    Added couple med DLL problems

---
+++ src/DoublyLinkedListMed/RemoveDupesFromSortedDLL.java
@@ -0,0 +1,29 @@
+package DoublyLinkedListMed;
+
+import FastAndSlowPointers.ListNode;
+
+public class RemoveDupesFromSortedDLL {
+
+    Node removeDuplicates(Node head) {
+        // Code Here.
+        if (head == null || head.next == null) return head;
+
+        // Iterate -> Skip dupes -> do the linkage
+        Node temp = head;
+        while (temp != null) {
+            Node firstOccurrence = temp;
+            // Skip the dupes
+            while (temp.next != null && temp.next.data == firstOccurrence.data) temp = temp.next;
+            // temp is at the last dupe so:
+            temp = temp.next;
+
+            // Do the linkage
+            firstOccurrence.next = temp;
+            if (temp != null)
+                temp.prev = firstOccurrence;
+        }
+
+        return head;
+    }
+
+}
