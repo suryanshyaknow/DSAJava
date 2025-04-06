0524883010af51a7b81885bb1f873d777598f948
Suryansh Grover <suryanshgrover1999@gmail.com>
2025-04-07 02:17:11 +0530

    Added optimal approach employing two pointers sliding window pattern for threeSum LC prob

--- src/TwoPointers/TripletSumToZero.java
+++ src/TwoPointers/TripletSumToZero.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class TripletSumToZero {
 
-    public static List<List<Integer>> searchTriplets(int[] arr) {
+    public static List<List<Integer>> searchTripletsDesignGurus(int[] arr) {
         List<List<Integer>> triplets = new ArrayList<>();
         // TODO: Write your code here
         // First off, sort the array
@@ -39,11 +39,52 @@ public class TripletSumToZero {
          */
     }
 
-    public List<List<Integer>> threeSum(int[] nums) {
+    public List<List<Integer>> searchTripletsOptimal(int[] nums) {
+        // The idea is to make an ele a pivot,
+        // and use two pointers approach to find the rem elements
+        // of the triplet.
+
+        // First off, to use to approach, given arr gotta be sorted
+        Arrays.sort(nums);
+        int N = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicate i
+
+            int leftPtr = i + 1;
+            int rightPtr = N - 1;
+            int target = -nums[i];
+
+            while (leftPtr < rightPtr) {
+                int sum = nums[leftPtr] + nums[rightPtr];
+                if (sum == target) {
+                    res.add(Arrays.asList(nums[i], nums[leftPtr], nums[rightPtr]));
+                    leftPtr++;
+                    rightPtr--;
+                    while (leftPtr < rightPtr && nums[leftPtr] == nums[leftPtr - 1]) leftPtr++;
+                    while (leftPtr < rightPtr && nums[rightPtr] == nums[rightPtr + 1]) rightPtr--;
+                } else if (sum < target) {
+                    leftPtr++;
+                    while (leftPtr < rightPtr && nums[leftPtr] == nums[leftPtr - 1]) leftPtr++;
+                } else {
+                    rightPtr--;
+                    while (leftPtr < rightPtr && nums[rightPtr] == nums[rightPtr + 1]) rightPtr--;
+                }
+            }
+        }
+        // Time: O(N Log N) + O(N^2)
+        // Space: O(N^2) (i.e. number of unique elements to return the ans)
+        return res;
+    }
+
+    public List<List<Integer>> searchTripletsBetter(int[] nums) {
         int N = nums.length;
         HashSet<List<Integer>> hashSet = new HashSet<>();
         for (int i = 0; i < N - 2; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) continue; // Skipping dupes for i
+
+            // From here on, the problem boils down to TwoSum ;)
             int target = -nums[i];
             HashMap<Integer, Integer> idxMap = new HashMap<>();
             for (int j = i + 1; j < N; j++) {
@@ -61,14 +102,19 @@ public class TripletSumToZero {
                 idxMap.put(nums[j], j);
             }
         }
+
+        // Time Complexity: O(N^2). Collections.sort(triplet) inside is O(3 log 3) → constant time, so ignorable.
+        // Or O(N^2 * log M) in case ordered set has been used. M being the size of unordered set (map) to find the 'remDuplet'.
+        // Space: O(N) (being used to store the elements while searching for 'remDuplet') + O(num of unique triplets) * 2 (Set as well as list)
+
         return new ArrayList<>(hashSet);
     }
 
     public static void main(String[] args) {
         int[] arr = {-3, 0, 1, 2, -1, 1, -2};
-        System.out.println(searchTriplets(arr));
+        System.out.println(searchTripletsDesignGurus(arr));
 
         int[] arr1 = {-1, 0, 1, 2, -1, -4};
-        System.out.println(searchTriplets(arr1));
+        System.out.println(searchTripletsDesignGurus(arr1));
     }
 }
