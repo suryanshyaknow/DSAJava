813d14b7c7cfdcd44eb3fa0bb152a8aab60afa9b
Suryansh Grover <suryanshgrover1999@gmail.com>
2025-07-11 09:17:23 +0530

    Added Stock Spanner prob

---
+++ src/Stack/StockSpanner.java
@@ -0,0 +1,34 @@
+package Stack;
+
+import java.util.Stack;
+
+public class StockSpanner {
+
+    private static class Pair {
+        private int idx;
+        private int val;
+
+        public Pair(int idx, int val) {
+            this.idx = idx;
+            this.val = val;
+        }
+    }
+
+    Stack<Pair> st;
+    int idx;
+
+    public StockSpanner() {
+        st = new Stack<>();
+        idx = -1;
+    }
+
+    public int next(int price) {
+        idx += 1;
+
+        while (!st.isEmpty() && st.peek().val <= price) st.pop();
+
+        int res = idx - (st.isEmpty() ? -1 : st.peek().idx);
+        st.push(new Pair(idx, price));
+        return res;
+    }
+}
