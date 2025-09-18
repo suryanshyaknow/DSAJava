456d7645c0a2148ed550dff65f74ec1c34a235c4
Suryansh Grover <suryanshgrover1999@gmail.com>
2025-09-18 09:56:29 +0530

    Added Disjoint Set union by rank as well as size

---
+++ src/MST/Medium/DisjointSetUnionByRank.java
@@ -0,0 +1,48 @@
+package MST.Medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DisjointSetUnionByRank {
+
+    List<Integer> rank = new ArrayList<>();
+    List<Integer> parent = new ArrayList<>();
+
+    public DisjointSetUnionByRank(int N) {
+        for (int i = 0; i < N; i++) {
+            rank.add(i, 0);
+            parent.add(i, i); // Initially, every node is parent of its own.
+        }
+    }
+
+    public int findUPar(int node) {
+        if (node == parent.get(node)) return node;
+
+//        return findUPar(parent.get(node));
+        // This woulda been logarithmic, but we also wanna do path compression for future calls to run in constant time.
+
+        // With path compression:
+        return parent.set(node, findUPar(parent.get(node)));
+    }
+
+    public void unionByRank(int u, int v) {
+        // Step 1. Find the ultimate parents of both nodes
+        // Step 2. Attach tree w smaller rank under the root of tree w larger rank
+        // Step 3. If ranks are equal, attach one under the other and increase the rank of new uPar w +1.
+        int uUPar = findUPar(u);
+        int vUPar = findUPar(v);
+        if (uUPar == vUPar) return; // both nodes already belongs to the same component
+
+        int uRank = rank.get(uUPar);
+        int vRank = rank.get(vUPar);
+        if (uRank < vRank)
+            parent.set(uUPar, vUPar); // Now, vUPar is the ultimate parent of even uUPar
+        else if (uRank > vRank)
+            parent.set(vUPar, uUPar);
+        else {
+            // Attach either one to another and increase the rank of new uPar
+            parent.set(uUPar, vUPar);
+            rank.set(vUPar, rank.get(vUPar) + 1);
+        }
+    }
+}
---
+++ src/MST/Medium/DisjointSetUnionUsingSize.java
@@ -0,0 +1,42 @@
+package MST.Medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DisjointSetUnionUsingSize {
+
+    List<Integer> size = new ArrayList<>();
+    List<Integer> parent = new ArrayList<>();
+
+    public DisjointSetUnionUsingSize(int N) {
+        for (int i = 0; i < N; i++) {
+            size.add(i, 1); // Initially, everyone's size is 1
+            parent.add(i, i); // Initially, every node is parent of its own.
+        }
+    }
+
+    public int findUPar(int node) {
+        if (node == parent.get(node)) return node;
+
+        // Path compression:
+        return parent.set(node, findUPar(parent.get(node)));
+    }
+
+    public void unionByRank(int u, int v) {
+        int uUPar = findUPar(u);
+        int vUPar = findUPar(v);
+        if (uUPar == vUPar) return; // both nodes already belongs to the same component
+
+        int uSize = size.get(uUPar);
+        int vSize = size.get(vUPar);
+        if (uSize < vSize) {
+            parent.set(uUPar, vUPar); // Now, vUPar is the ultimate parent of even uUPar
+            size.set(vUPar, size.get(vUPar) + 1);
+        } else {
+            // Attach either one to another and increase the size of new uPar
+            parent.set(vUPar, uUPar);
+            size.set(uUPar, size.get(uUPar) + 1);
+        }
+    }
+
+}
