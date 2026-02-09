bb38fa2c03c4b2a1cd7701c77a0b8bb443027dc0
Suryansh Grover <suryanshgrover1999@gmail.com>
2026-02-10 02:10:30 +0530

    Readded Max Path Sum in BT module

--- 79LastMoment/src/BinaryTree/MaximumPathSum.java
+++ 79LastMoment/src/BinaryTree/MaximumPathSum.java
@@ -2,33 +2,44 @@ package BinaryTree;
 
 public class MaximumPathSum {
 
-    private int maxPathSum = Integer.MIN_VALUE;
+    int maxPathSum = Integer.MIN_VALUE;
 
     public int maxPathSum(TreeNode root) {
+        // Traverse each node and treat it as
+        // curvature of the path, and compute the
+        // left path sum and right path sum and add the
+        // node's own val.
+
         if (root == null) return 0;
-
-        // Intuition: Pretty simple. Traverse thru each node
-        // computing the max of left and right gain. But while
-        // backtracking updates the max path sum for the node
-        // being treated as pivot at a recursive call.
-
-        // Do a post-order traversal.
-        // Each node returns the maximum gain of a single path going upward.
-        // While backtracking, treat the current node as a pivot and
-        // update the global maximum path sum using both left and right gains.
-        // Negative gains are ignored since a path is optional.
-        dfs(root);
+        computeHeightSum(root);
         return maxPathSum;
     }
 
-    private int dfs(TreeNode root) {
+//    private void dfs(TreeNode root) {
+//        if (root == null) return;
+//
+//        int lHeightSum = Math.max(0, computeHeightSum(root.left)); // cuz node's val could be negative as well and it's not clamped in computeHeightSum method
+//        int rHeightSum = Math.max(0, computeHeightSum(root.right));
+//        int pathSum = root.val + lHeightSum + rHeightSum;
+//        maxPathSum = Math.max(maxPathSum, pathSum);
+//
+//        dfs(root.left);
+//        dfs(root.right);
+//    }
+
+    private int computeHeightSum(TreeNode root) {
         if (root == null) return 0;
 
-        int leftGain = Math.max(0, dfs(root.left));
-        int rightGain = Math.max(0, dfs(root.right));
-        maxPathSum = Math.max(maxPathSum, root.val + leftGain + rightGain);
+        // We don't want any negative contributions
+        int lhSum = Math.max(0, computeHeightSum(root.left));
+        int rhSum = Math.max(0, computeHeightSum(root.right));
 
-        return root.val + Math.max(leftGain, rightGain);
+        // Now think about it, to compute path sum we need to have lhSum and rhSum, so
+        // we don't really need to compute it dedicately for each node, we'd leverage
+        // recursion, i.e., the nodes being processed first in the recursion
+        maxPathSum = Math.max(maxPathSum, root.val + lhSum + rhSum);
+
+        return root.val + Math.max(lhSum, rhSum);
     }
 
 }
