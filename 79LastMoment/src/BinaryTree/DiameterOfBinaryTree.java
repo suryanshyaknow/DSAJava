61a41d9d767a2f5a24c0dcdc43934b46259782a4
Suryansh Grover <suryanshgrover1999@gmail.com>
2026-02-10 01:42:40 +0530

    Readded dia of BT

--- 79LastMoment/src/BinaryTree/DiameterOfBinaryTree.java
+++ 79LastMoment/src/BinaryTree/DiameterOfBinaryTree.java
@@ -2,44 +2,40 @@ package BinaryTree;
 
 public class DiameterOfBinaryTree {
 
-    int dia = 0; // If you declare it static it'll track the max from prev instances
+    private int maxLen = 0; // dia; longest path bw any two nodes
 
     public int diameterOfBinaryTree(TreeNode root) {
         if (root == null) return 0;
 
-        // The idea is pretty simple: Treat each node as
-        // a curve point and compute the lh and the rh,
-        // and their summation gives us the distance passing
-        // thru that node.
-        // you've actually gotta compute the left max height
-        // and the right max height
-
-        recurseAndCompute(root);
-        return dia;
-
-        // Note: Some problems are not about “computing for each node”,
-        // they’re about “updating the answer while traversing once”.
-
-        // Because approach 1 (brute force) -> Process each node starting off w root,
-        // and compute the dia for each and keep updating the max.
-
-        // Optimal (O(N) approach) -> Not about the order of execution of nodes. Just
-        // keep updating max dia starting off from leaf nodes. This lets you compute the max
-        // by traversing only once.
+        computeHeight(root);
+        return maxLen;
     }
 
-    private int recurseAndCompute(TreeNode root) {
+    // private void dfs(TreeNode root) {
+    //     if (root == null) return;
+
+    //     // Compute the dia passing thru each node
+    //     // Preorder, since root is being processed first off
+    //     int lHeight = computeHeight(root.left);
+    //     int rHeight = computeHeight(root.right);
+    //     int currDia = lHeight + rHeight;
+    //     maxLen = Math.max(currDia, maxLen);
+
+    //     dfs(root.left);
+    //     dfs(root.right);
+    // }
+
+    private int computeHeight(TreeNode root) {
         if (root == null) return 0;
 
-        int lh = recurseAndCompute(root.left);
-        int rh = recurseAndCompute(root.right);
+        int leftHeight = computeHeight(root.left);
+        int rightHeight = computeHeight(root.right);
 
-        int path = lh + rh;
-        // Update the dia
-        dia = Math.max(dia, path);
+        // Now for this node being processed, we've its left
+        // height as well as right height, it's dia could be computed
+        maxLen = Math.max(maxLen, leftHeight + rightHeight);
 
-        // return the height
-        return 1 + Math.max(lh, rh);
+        return 1 + Math.max(leftHeight, rightHeight);
     }
 
 }
