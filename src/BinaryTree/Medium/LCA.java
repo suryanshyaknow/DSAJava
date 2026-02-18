f5dd7df5f3b3a41c4a6e039ce2344192bbd24228
Suryansh Grover <suryanshgrover1999@gmail.com>
2026-02-19 01:49:46 +0530

    Revisited LCA in binary tree

--- src/BinaryTree/Medium/LCA.java
+++ src/BinaryTree/Medium/LCA.java
@@ -10,14 +10,23 @@ public class LCA {
         // If the flow reached at one of the nodes we're looking for, return that node
         // If a node returns both the nodes we're looking at, return the node.
 
-        if (root == null || root == p || root == q) return root;
+        if (root == null) return root;
+
+        // If the curr node is one of the targets, return it upwards
+        if (root == p || root == q) return root;
 
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
 
-        if (left == null) return right;
-        else if (right == null) return left;
-        else return root;
+        if (left != null && right != null) return root;
+
+        if (left != null) return left;
+        else if (right != null) return right;
+        else return null;
+
+//        if (left == null) return right;
+//        else if (right == null) return left;
+//        else return root;
     }
 
 }
