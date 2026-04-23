8e49403fa747145210ddb113144a450796617fbe
Suryansh Grover <suryanshgrover1999@gmail.com>
2026-04-23 23:19:53 +0530

    Added tabulation approach of the target sum pron

--- 79LastMoment/src/DynamicProgramming/DPOnSubsequences/TargetSumEqualsK.java
+++ 79LastMoment/src/DynamicProgramming/DPOnSubsequences/TargetSumEqualsK.java
@@ -4,6 +4,82 @@ import java.util.*;
 
 public class TargetSumEqualsK {
 
+    public int findTargetSumWaysTabulation(int[] nums, int target) {
+        // See, if the S is the sum of all the integers in arr
+        // target is given
+        // s1 + s2 = S; because + or - are to be put before each ele
+        // s1 - s2 = target
+        // 2s1 = S + target
+        // s1 = (S + target) / 2
+
+        int S = 0;
+        int N = nums.length;
+        for (int i = 0; i < N; i++) {
+            S += nums[i];
+        }
+
+        int sumTotal = S + target;
+        if (sumTotal < 0 || sumTotal % 2 != 0) return 0;
+        int newTarget = (S + target) / 2;
+
+        int[][] dp = new int[N][newTarget + 1];
+
+        for (int i=0; i < N; i++) {
+            for (int t=0; t <= newTarget; t++) {
+                // Edge case
+                if (i == 0) { // cuz we gotta cover all the elements
+                    if (t == 0 && nums[i] == 0) {
+                        dp[i][t] = 2;
+                        continue;
+                    } else if (t == 0 || nums[i] == t) {
+                        dp[i][t] = 1;
+                        continue;
+                    } else {
+                        dp[i][t] = 0;
+                        continue;
+                    }
+                }
+
+                // pick
+                int pick = 0;
+                if (nums[i] <= t)
+                    pick = dp[i - 1][t - nums[i]];
+                // not pick
+                int notPick = dp[i - 1][t];
+
+                dp[i][t] = pick + notPick;
+            }
+        }
+
+        return dp[N - 1][newTarget];
+
+        // Time: O(N * t)
+        // Space: O(N * t)
+    }
+
+    private int helper(int i, int[] nums, int t, int[][] dp) {
+        // Since we're to find all the subsets, we
+        // oughta ensure that we cover all the elements
+        // and not early return.
+
+        if (i == 0) {
+            if (t == 0 && nums[i] == 0)
+                return 2;
+            if (t == 0 || nums[i] == t)
+                return 1;
+            return 0;
+        }
+
+        if (dp[i][t] != -1) return dp[i][t];
+
+        int notPick = helper(i - 1, nums, t, dp);
+        int pick = 0;
+        if (nums[i] <= t)
+            pick += helper(i - 1, nums, t - nums[i], dp);
+
+        return dp[i][t] = pick + notPick;
+    }
+
     public int findTargetSumWaysMemo(int[] nums, int target) {
         // See, if the S is the sum of all the integers in arr
         // target is given
